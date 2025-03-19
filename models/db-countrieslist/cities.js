13d147a284883a0ff2963a4ff954cb3ddbd0fe5f
KatangoorSrikithReddy <135942314+KatangoorSrikithReddy@users.noreply.github.com>
2025-03-19 13:57:37 +0530

    applyied city data

--- models/db-countrieslist/cities.js
+++ models/db-countrieslist/cities.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.UUID,
       allowNull: false,
       references: {
-        model: "State",
+        model: "States",
         key: "id"
       }
     },
